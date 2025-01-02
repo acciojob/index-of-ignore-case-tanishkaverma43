b583454925cacfaa68a7506ca2a57b0b6f9ad834
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 16:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	
+  var lowerS1 = s1.toLowerCase();
+  var lowerS2 = s2.toLowerCase();
+  
+  var index = lowerS1.indexOf(lowerS2);
+
+	return index;
+	
 }
 
 // Please do not change the code below
